bfc718a057678f65b401b54d531b79574e66a6e4
Robert Okon <okonpersonal@gmail.com>
2012-06-07 21:37:18 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Countrycabs::Application.config.secret_token = 'fcbbc0ada2f924317ff9c1f7e812b631019d6fcd4e58949e015fece436020fbc1864b2e1b4b9f2962e7a206c9c34f5ca3af53db3f5d21646043fdc978963c19e'
